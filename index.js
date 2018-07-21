3f5eb1dc476bdb065db32630c9cca07b3493fe84
User <user@Users-Air.lan>
2018-07-21 16:59:16 +0300

    add env port

--- index.js
+++ index.js
@@ -59,4 +59,4 @@ app.post('/email', (req, res) => {
 
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(rocess.env.PORT | 3000, () => console.log('Example app listening on port 3000!'));
